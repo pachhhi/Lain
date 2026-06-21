50dec3df1ddd0a891d540db88fd7aa83c4b3ebba
pachhhi <carrizotomasss@outlook.com>
2026-06-21 20:40:04 -0300

    v1 - added core/manager

---
+++ core/manager.py
@@ -0,0 +1,33 @@
+from core.providers.system import SystemProvider
+# from core.providers.memory import MemoryProvider
+# from core.providers.history import HistoryProvider
+
+class ContextManager:
+
+    def build(self, prompt, mode):
+
+        context = []
+
+        context.append(
+            SystemProvider().get_context()
+            # print(SystemProvider().get_context())
+        )
+
+        # context.append(
+        #     MemoryProvider().get_context()
+        # )
+
+        # context.append(
+        #     HistoryProvider().get_context()
+        # )
+
+        # context.append(
+        #     f"USER:\n{prompt}"
+        # )
+
+        print(context)
+
+        return "\n\n".join(
+            block for block in context
+            if block
+        )
